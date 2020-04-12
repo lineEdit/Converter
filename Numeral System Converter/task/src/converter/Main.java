3cc6faefcbbfe13cfda4284bbb1747d9a05fac8f
lineedit <lineedit@gmail.com>
2020-04-12 16:42:00 +0500

    Stage 3, end. Input 2 number, values and radix, sout = result

--- Numeral System Converter/task/src/converter/Main.java
+++ Numeral System Converter/task/src/converter/Main.java
@@ -6,7 +6,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int value = scanner.nextInt();
-        String octal = Integer.toOctalString(value);
-        System.out.println(octal.substring(octal.length() - 1));
+        int radix = scanner.nextInt();
+        String result = "";
+        switch (radix) {
+            case 2:
+                result = "0b" + Integer.toBinaryString(value);
+                break;
+            case 8:
+                result = "0" + Integer.toOctalString(value);
+                break;
+            case 16:
+                result = "0x" + Integer.toHexString(value);
+                break;
+        }
+        System.out.println(result);
     }
 }
