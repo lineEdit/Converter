155730f197ccfc718d563e0db688b01d3dca9676
lineedit <lineedit@gmail.com>
2020-04-12 16:21:37 +0500

    Stage 2, end. Convert 10 to 8 and show last symbol.

--- Numeral System Converter/task/src/converter/Main.java
+++ Numeral System Converter/task/src/converter/Main.java
@@ -1,7 +1,12 @@
 package converter;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        int value = scanner.nextInt();
+        String octal = Integer.toOctalString(value);
+        System.out.println(octal.substring(octal.length() - 1));
     }
 }
