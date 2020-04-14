d11addccf2630006b539025a3209ff1ca6a20815
lineedit <lineedit@gmail.com>
2020-04-14 18:25:13 +0500

    Stage 5, testing, parse input values, catch errors

--- Numeral System Converter/task/src/converter/Main.java
+++ Numeral System Converter/task/src/converter/Main.java
@@ -3,13 +3,37 @@ package converter;
 import java.util.*;
 
 public class Main {
+    private static final int SOURCE_RADIX = 0;
+    private static final int SOURCE_NUMBER = 1;
+    private static final int TARGET_RADIX = 2;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int sourceRadix = scanner.nextInt();
-        String sourceNumber = scanner.next();
-        int targetRadix = scanner.nextInt();
+        List<String> list = new ArrayList<>();
+        while (scanner.hasNextLine()) {
+            list.add(scanner.nextLine());
+        }
 
-        Converter converter = new Converter(sourceRadix, sourceNumber, targetRadix);
-        converter.calc();
+        if (list.size() != 3) {
+            System.out.println("Error input count values");
+            return;
+        }
+        if (list.get(SOURCE_RADIX).matches("\\d+")
+                && list.get(TARGET_RADIX).matches("\\d+")) {
+            int sourceRadix = Integer.parseInt(list.get(SOURCE_RADIX));
+            int targetRadix = Integer.parseInt(list.get(TARGET_RADIX));
+            if (sourceRadix > 0 && targetRadix > 0
+                    && sourceRadix < 37 && targetRadix < 37) {
+                Converter converter = new Converter(sourceRadix
+                        , list.get(SOURCE_NUMBER)
+                        , targetRadix);
+                converter.calc();
+            } else {
+                System.out.println("Error input (sourceRadix or targetRadix) < 1 " +
+                        "or (sourceRadix < 37 && targetRadix < 37)");
+            }
+        } else {
+            System.out.println("Error input (sourceRadix or targetRadix) != integer");
+        }
     }
 }
